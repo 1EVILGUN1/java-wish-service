9522325c71dc32d733694640a3905d7bdd2797e6
YG <yasha.golotin@mail.ru>
2025-05-11 11:55:43 +0300

    feat: add mapstruct

--- src/main/java/pet/project/wish/service/impl/UserServiceImpl.java
+++ src/main/java/pet/project/wish/service/impl/UserServiceImpl.java
@@ -9,7 +9,7 @@ import pet.project.wish.dto.user.UserAuthDto;
 import pet.project.wish.dto.user.UserRequestCreatedDto;
 import pet.project.wish.dto.user.UserResponseDto;
 import pet.project.wish.error.NotFoundException;
-import pet.project.wish.mapper.UserMapper;
+import pet.project.wish.mapper.UseMapper;
 import pet.project.wish.model.User;
 import pet.project.wish.repository.UserRepository;
 import pet.project.wish.service.UserService;
@@ -24,14 +24,15 @@ import java.util.ArrayList;
 public class UserServiceImpl implements UserService {
     private final TransactionalOperator transactionalOperator;
     private final UserRepository repository;
-    private final UserMapper mapper;
+    private final UseMapper mapper;
 
     @Override
     public Mono<UserResponseDto> create(UserRequestCreatedDto dto) {
         return Mono.defer(() -> mapper.mapToUserCreatedDto(dto))
                 .flatMap(repository::save)
-                .switchIfEmpty(Mono.error(new NotFoundException("User not found")))  // Сохраняем реактивно
-                .flatMap(mapper::mapToUser) // Преобразуем в UserDto
+                .switchIfEmpty(Mono.error(new NotFoundException("User not found")))
+                .flatMap(user -> mapper.mapToUserResponseDto(Mono.just(user)))
+                .flatMap(Mono::just) // Разворачиваем Mono<Mono<UserResponseDto>> в Mono<UserResponseDto>
                 .as(transactionalOperator::transactional);
     }
 
@@ -40,26 +41,29 @@ public class UserServiceImpl implements UserService {
         return Mono.defer(() -> mapper.mapToUserCreatedDto(dto))
                 .doOnNext(user -> user.setId(id))
                 .flatMap(repository::save)
-                .flatMap(mapper::mapToUser)
+                .flatMap(user -> mapper.mapToUserResponseDto(Mono.just(user)))
+                .flatMap(Mono::just)
                 .as(transactionalOperator::transactional);
-
     }
 
     @Override
     public Mono<UserResponseDto> getId(Long id) {
         return repository.findById(id)
                 .switchIfEmpty(Mono.error(new NotFoundException("User not found")))
-                .flatMap(mapper::mapToUser);
+                .flatMap(user -> mapper.mapToUserResponseDto(Mono.just(user)))
+                .flatMap(Mono::just);
     }
 
     @Override
     public Flux<UserResponseDto> getAll() {
-        return repository.findAll().flatMap(mapper::mapToUser);
+        return repository.findAll()
+                .flatMap(user -> mapper.mapToUserResponseDto(Mono.just(user)))
+                .flatMap(Mono::just);
     }
 
     @Override
     public Mono<Void> delete(Long id) {
-           return repository.deleteById(id);
+        return repository.deleteById(id);
     }
 
     @Override
@@ -67,7 +71,8 @@ public class UserServiceImpl implements UserService {
         return repository.findUserByNameAndPassword(dto.name(), dto.password())
                 .log("User login" + dto)
                 .switchIfEmpty(Mono.error(new NotFoundException("User not found")))
-                .flatMap(mapper::mapToUser);
+                .flatMap(user -> mapper.mapToUserResponseDto(Mono.just(user)))
+                .flatMap(Mono::just);
     }
 
     @Override
@@ -81,31 +86,30 @@ public class UserServiceImpl implements UserService {
     public Mono<UserResponseDto> getFriend(Long userId, Long friendId) {
         return repository.getFriend(userId, friendId)
                 .switchIfEmpty(Mono.error(new NotFoundException("Friend not found")))
-                .transform(mapper::mapToFriendUserDto);
+                .flatMap(user -> mapper.mapToFriendUserDto(Mono.just(user)))
+                .flatMap(Mono::just);
     }
 
     @Override
     public Mono<Void> addPresent(Long userId, Long presentId) {
-        return findByIdUserInDatabase(userId)// Предполагается, что есть метод mapToUserEntity
-                            .flatMap(user -> {
-                                // Добавляем presentId в список, если его там нет
-                                if(user.getPresentIds() == null || user.getPresentIds().isEmpty()) {
-                                    user.setPresentIds(new ArrayList<>());
-                                }
-                                if (!user.getPresentIds().contains(presentId)) {
-                                    user.getPresentIds().add(presentId);
-                                }
-                                // Сохраняем обновленного пользователя
-                                return repository.save(user);
-                            })
-                .then() // Преобразуем в Mono<Void>
-                .as(transactionalOperator::transactional) // Обеспечиваем транзакционность
+        return findByIdUserInDatabase(userId)
+                .flatMap(user -> {
+                    if (user.getPresentIds() == null || user.getPresentIds().isEmpty()) {
+                        user.setPresentIds(new ArrayList<>());
+                    }
+                    if (!user.getPresentIds().contains(presentId)) {
+                        user.getPresentIds().add(presentId);
+                    }
+                    return repository.save(user);
+                })
+                .then()
+                .as(transactionalOperator::transactional)
                 .onErrorResume(e -> {
                     if (e instanceof NotFoundException) {
-                        return Mono.error(e); // Пробрасываем NotFoundException как есть
+                        return Mono.error(e);
                     }
-                    log.error("Error removing present", e);
-                    return Mono.error(new RuntimeException("Failed to remove present", e));
+                    log.error("Error adding present", e);
+                    return Mono.error(new RuntimeException("Failed to add present", e));
                 });
     }
 
@@ -113,17 +117,12 @@ public class UserServiceImpl implements UserService {
     public Mono<Void> removePresent(Long userId, Long presentId) {
         return findByIdUserInDatabase(userId)
                 .flatMap(user -> {
-                    // Инициализируем список, если он null
                     if (user.getPresentIds() == null) {
                         user.setPresentIds(new ArrayList<>());
                     }
-
-                    // Проверяем наличие подарка
                     if (!user.getPresentIds().contains(presentId)) {
                         return Mono.error(new NotFoundException("Present not found"));
                     }
-
-                    // Удаляем подарок
                     user.getPresentIds().remove(presentId);
                     return repository.save(user);
                 })
@@ -131,7 +130,7 @@ public class UserServiceImpl implements UserService {
                 .as(transactionalOperator::transactional)
                 .onErrorResume(e -> {
                     if (e instanceof NotFoundException) {
-                        return Mono.error(e); // Пробрасываем NotFoundException как есть
+                        return Mono.error(e);
                     }
                     log.error("Error removing present", e);
                     return Mono.error(new RuntimeException("Failed to remove present", e));
@@ -147,30 +146,22 @@ public class UserServiceImpl implements UserService {
                 .flatMap(tuple -> {
                     User user = tuple.getT1();
                     User friend = tuple.getT2();
-
-                    // Инициализация списка друзей если нужно
                     if (user.getFriendsIds() == null) {
                         user.setFriendsIds(new ArrayList<>());
                     }
-
-                    // Добавляем друга если его еще нет в списке
                     if (!user.getFriendsIds().contains(friendId)) {
                         user.getFriendsIds().add(friendId);
                         return repository.save(user);
                     }
-
                     return Mono.just(user);
                 })
                 .then()
                 .onErrorResume(e -> {
-                    // Логируем ошибку и возвращаем Mono.error или Mono.empty
                     log.error("Error adding friend", e);
                     return Mono.error(new RuntimeException("Failed to add friend"));
                 })
                 .as(transactionalOperator::transactional);
     }
-
-
 
     private Mono<User> findByIdUserInDatabase(Long id) {
         return repository.findById(id)
